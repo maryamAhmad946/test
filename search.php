4ea911d73be9303d24282b66cf9189b451b8e73e
maryamAhmad946 <72168845+maryamAhmad946@users.noreply.github.com>
2022-08-25 17:02:15 -0700

    updates

--- search.php
+++ search.php
@@ -1,6 +1,17 @@
 <?php
 
 if (isset($argc)) {
+
+    $file = $argv[1];
+    $index = $argv[2];
+    if ($argv[2] == 0) {
+        echo "lesser index not found";
+        exit;
+    } else {
+        $index = $argv[2] - 1;
+    }
+    //echo $index;
+    //exit;
     if (($open = fopen($argv[1], "r")) !== FALSE) {
 
         while (($data = fgetcsv($open, 1000, ",")) !== FALSE) {
@@ -9,8 +20,17 @@ if (isset($argc)) {
 
         fclose($open);
     }
-    print_r($array[$argv[2]]);
+
+    $total_records = count($array);
+
+    if ($index >= $total_records) {
+        echo "greater index not found";
+    } else {
+        echo $array[$index][0] . ","; // id
+        echo $array[$index][1] . ","; // first name
+        echo $array[$index][2] . ","; // last name
+        echo $array[$index][3]; // dob
+    }
 } else {
-    
     echo "argc and argv disabled\n";
 }
